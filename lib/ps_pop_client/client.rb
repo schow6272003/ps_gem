543511c84fd21b8e9b8581ae8e25d7a2b53770db
steven.chow <stevenwchow@gmail.com>
2019-09-19 17:17:50 -0700

    Added check_request helper to validate input

--- lib/ps_pop_client/client.rb
+++ lib/ps_pop_client/client.rb
@@ -6,9 +6,11 @@ module PSClient
       super(args)
     end 
 
-    def find
+    def find(req={}) 
       begin
-       response = request("/api/cbsa?cbsa_ids[]=11260")
+        check_request(req)
+       uri = encode_uri(req)
+       response = request(uri)
         check_status(response)
         return Response.new(response).parse
       rescue => e
@@ -16,15 +18,28 @@ module PSClient
       end
     end 
 
-    def find_all
-    
-    end 
-
     private      
       def check_status(response)
         return if (200..299).cover?(response.code.to_i)
         raise Error.new(response.message, response.code)
       end
+      
+      def check_request(req)
+        if !req[:cbsa_ids] && !req[:zipcodes] && !req[:name] 
+          raise Error.new("Bad Request", 400)
+        elsif ( req[:cbsa_ids] && !req[:cbsa_ids].kind_of?(Array)) 
+              || ( req[:zipcodes] && !req[:zipcodes].kind_of?(Array))
+              || ( req[:name] && req[:name].kind_of?(String)) 
+          raise Error.new("Bad Request", 400)
+        end 
+      end 
+
+      def encode_uri(req)
+        cbsa_ids_uri = req[:cbsa_ids].inject("") { |s, a| s + "cbsa_ids[]=" + a.to_s + "&" }
+        zipcodes_uri = req[:zipcodes].inject("") { |s, a| s + "zicodes[]=" + a.to_s + "&" }
+        name = "name="  + rq[:name]
+        "/api/cbsa?" + cbsa_ids_uri + zipcodes_uri + name
+      end 
   end
 end 
 
