432d3ebe79362e65528e49343941cb436692deec
steven.chow <stevenwchow@gmail.com>
2019-09-24 08:25:25 -0700

    modified error message contents

--- lib/ps_pop_client/tools/error_util.rb
+++ lib/ps_pop_client/tools/error_util.rb
@@ -1,9 +1,9 @@
 module ErrorUtil
   def self.parse_response(e)
-     response = {
-       status: (e.class == PSClient::Error) ?  e.code : 500,
-       message: e.message
-     }
+    response = {
+      status: (e.class == PSClient::Error) ?  e.code.to_i : 500,
+      message: e.message
+    }
     return response
   end 
 end 
--- spec/ps_pop_client_spec.rb
+++ spec/ps_pop_client_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PSClient::Api do
   before do
-    @client = described_class.new
+    @client = described_class.new({ :base_uri => "http://localhost:8000" })
   end  
 
   describe "#find"  do
@@ -10,32 +10,31 @@ describe PSClient::Api do
 
     context "when valid request params" do 
       let(:req) { { "cbsa_ids" => [11260] }} 
-    
       it "expects to return with 200 status code" do
-        expect(subject[:status]).to eq "200"
+        expect(subject[:status]).to eq 200
       end 
       
       it "expects to return 1 record" do
-        expect(subject[:body].count).to eq 1
+        expect(subject[:data]["records"].count).to eq 1
       end
       
       it "expects cbsa code to be 11260" do
-       expect(subject[:body][0]["cbsa_id"]).to eq "11260"
+       expect(subject[:data]["records"][0]["cbsa_id"]).to eq 11260
       end 
 
       it "expects to include zipcodes" do
-        expect(subject[:body][0]["zip_code"]).to include(99501,99502,99503)
+        expect(subject[:data]["records"][0]["zip_code"]).to include(99501,99502,99503)
       end 
       
       it "expects to include msa name" do
-        expect(subject[:body][0]["name"]).to eq "Anchorage, AK"
+        expect(subject[:data]["records"][0]["name"]).to eq "Anchorage, AK"
       end
 
       it "expects to include 2014 pop estimate" do 
-        expect(subject[:body][0]["pop_estimate"][4]["number"]).to eq "398642"
+        expect(subject[:data]["records"][0]["pop_estimate"][4]["number"]).to eq 398642
       end
       it "expects to include 2015 pop estimate" do 
-        expect(subject[:body][0]["pop_estimate"][5]["number"]).to eq "399790"
+        expect(subject[:data]["records"][0]["pop_estimate"][5]["number"]).to eq 399790
       end
     end  
 
@@ -43,19 +42,19 @@ describe PSClient::Api do
       let(:req) { { :zip_codes => [48813,5482] }} 
     
       it "expects to return with 200 status code" do
-        expect(subject[:status]).to eq "200"
+        expect(subject[:status]).to eq 200
       end 
 
       it "expects to return 2 record" do
-        expect(subject[:body].count).to eq 2
+        expect(subject[:data]["count"]).to eq 2
       end
 
       it "expects first record to have zipcode 48813" do
-        expect(subject[:body][0]["zip_code"]).to include(48813)
+        expect(subject[:data]["records"][0]["zip_code"]).to include(48813)
       end
 
       it "expects first record to have zipcode 5482" do
-        expect(subject[:body][1]["zip_code"]).to include(5482)
+        expect(subject[:data]["records"][1]["zip_code"]).to include(5482)
       end
     end  
 
@@ -63,14 +62,14 @@ describe PSClient::Api do
       let(:req) { { :name => "Burlington" }} 
 
       it "expects to return with 200 status code" do
-        expect(subject[:status]).to eq "200"
+        expect(subject[:status]).to eq 200
       end 
 
       it "expects to return 2 record" do
-        expect(subject[:body].count).to eq 2
+        expect(subject[:data].count).to eq 2
       end
       it "expects to include 'Burlington' on msa name" do
-        expect(subject[:body][0]["name"]).to include("Burlington")
+        expect(subject[:data]["records"][0]["name"]).to include("Burlington")
       end
     end 
 
@@ -78,11 +77,11 @@ describe PSClient::Api do
       let (:req) {{:name => "Hello World!"}}
       
       it "expects to return with 200 status code" do
-        expect(subject[:status]).to eq "200"
+        expect(subject[:status]).to eq 200
       end
       
       it "expects to return 0 records" do
-         expect(subject[:body].count).to eq 0
+         expect(subject[:data]["records"].count).to eq 0
       end 
     end
 
